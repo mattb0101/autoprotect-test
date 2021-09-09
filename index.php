b22b523a6df0057d73ea43026501d01bc1af14c5
mattb0101 <mattbaum666@googlemail.com>
2021-09-09 15:10:24 +0000

    Initial Commit, project created

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Auto Protect Code Test</title>
+</head>
+<body>
+    
+    <h1><?php echo 'hello world'; ?></h1>
+
+</body>
+</html>
