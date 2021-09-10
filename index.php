2b46c59f61006e27ae9bf5dac150facb6badba69
mattb0101 <mattbaum666@googlemail.com>
2021-09-10 10:08:30 +0000

    Added in for loops for creating the table with 2 variables

--- index.php
+++ index.php
@@ -17,8 +17,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    
-    <h1><?php echo 'hello world'; ?></h1>
 
     <div class="table-responsive w-50">
         <table class="table caption-top">
@@ -29,9 +27,14 @@
                 for($i = 1; $i < $row +1; $i++){
                     echo '<tr>';
                         
-                        for($j = 1; $j < $col +1; $j++) 
-                            echo '<td>' .$i*$j . '</td>';
-                        echo '</tr>';
+                        for($j = 1; $j < $col +1; $j++) {
+                            if($j == $i){
+                                echo '<td class="fw-bold">' .$i*$j . '</td>';
+                            } else{
+                                echo '<td>' .$i*$j . '</td>';
+                            }
+                        }
+                        echo '</tr>';     
                     }
                 ?>
             
